dbca91beece227f2204ed1a994107aba637be4f3
LeeYuSeung <akows141@gmail.com>
2022-08-07 16:44:51 +0900

    [로그인 상태 판별] - 최상단 App 페이지에서 로그인 여부를 검증하여 그 결과를 필요한 페이지에 전달하는 기능 구현.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 
@@ -10,10 +11,27 @@ import Board from './pages/board';
 import Member from './pages/member';
 
 function App() {
+
+  const [isLogin, setIsLogin] = useState(false)
+
+  useEffect(() => {
+      // sessionStorage에 Item으로 담겨진 로그인 값을 검증.
+      // 로그인 값의 이름은 'MEMBER_ID', 이 값이 없을(null) 경우 아무 일도 일어나지 않는다.
+      if (sessionStorage.getItem('MEMBER_ID') === null) {
+          console.log('isLogin ?? :: ', isLogin)
+      } 
+      else {
+      // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
+      // 로그인 상태 변경
+          setIsLogin(true)
+          console.log('isLogin ?? :: ', isLogin)
+      }
+  }, [isLogin])
+
   return (
     <>
       <BrowserRouter>
-        <Menubar/>
+        <Menubar isLogin={isLogin}/>
         <Routes>
           <Route path='/' element={<Main/>}/>
           <Route path='/api' element={<Callapi/>}/>
