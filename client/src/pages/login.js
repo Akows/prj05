961039a5f9ae641c0cdbd093e794d2440bfd7708
LeeYuSeung <akows141@gmail.com>
2022-08-18 21:50:33 +0900

    [JWTWebToken 설치] [Express CORS 설치] - JWT 로그인 시스템 구축을 위해 모듈 설치. - 백엔드 사이드 CORS 문제 해결을 위한 모듈 설치. (이미 프론트단에서 처리되고있으므로 설치 후 사용하지는 않음.)

---
+++ client/src/pages/login.js
@@ -0,0 +1,30 @@
+import '../style/Login.css';
+import '../style/GlobalStyle.css';
+
+const Login = () => {
+
+    return (
+        <>
+            <div className='login-pagebackground setcenter'>
+                <div className='login-pageinner setcenter'>
+                    <div className='login-formarea gifont'>
+
+                        <div className='login-title setcenter'>
+                            <h1>사용자 로그인</h1>
+                        </div>
+
+                        <div className='login-input'>
+                            <input/>
+                            <input/>
+                        </div>
+
+
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
