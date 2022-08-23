72b9cbf1758cc974c0de0677a61ef372650acbd1
LeeYuSeung <akows141@gmail.com>
2022-08-23 23:14:28 +0900

    [코드 정리] - 필요한 부분에 주석 추가.

--- server/JWT.js
+++ server/JWT.js
@@ -1,12 +1,12 @@
-// JWT와 관련된 코드들이 작성될 파일.
-
+// JWT와 관련된 코드가 작성될 파일.
 const { sign, verify } = require("jsonwebtoken");
 
 // JWT를 생성하는 함수.
 // member 정보를 매개변수로 받아 토큰을 생성하여 반환한다.
 const createTokens = (user) => {
     const accessToken = sign({ memberid: user.MEMBER_ID, memberpw: user.MEMBER_PW }, "jwtsecretplschange");
-        return accessToken;
+    
+    return accessToken;
 };
 
 // JWT를 검증하는 함수.
