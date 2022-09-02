56cf84a611b4e0a9e19fb4adc284a871cd4835c3
LeeYuSeung <akows141@gmail.com>
2022-09-02 18:19:21 +0900

    [JWT 로그인] - React Cookie 사용을 위한 Cookie.js 파일 추가. - App.js 파일에서 React Cookie 사용처리. - 이전 로그인 방법을 사용하는 코드 삭제.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { createContext, useState } from 'react';
 
+import { CookiesProvider } from 'react-cookie';
+
 import './App.css';
 
 import Uppermenu from './components/uppermenu';
@@ -12,7 +14,7 @@ import BoardViewandMod from './pages/boardviewandmod';
 import Login from './pages/login';
 import Join from './pages/join';
 import MemberInfo from './components/memberinfo';
-import { useCookies } from 'react-cookie';
+
 import Test from './pages/test';
 
 // 전역 상태 관리를 위한 ContextAPI 사용을 위해 createContext()을 사용, 빈 Context를 생성.
@@ -25,51 +27,30 @@ function App() {
 
     // 로그인 상태를 제어하는 loginStatus.
     const [loginStatus, setLoginStatus] = useState(false);
-
     // 로그인한 유저의 아이디 값을 제어하는 whoIsLogin.
     const [whoIsLogin, setwhoIsLogin] = useState('비로그인사용자');
 
-    // 로그인한 유저의 쿠키 값을 제어하는 cookies.
-    const [cookies, setCookie, removeCookie] = useCookies(['쿠키 이름']);
-
-  // Login 페이지에서 App으로 로그인 값을 넘겨줄 함수.
-  const receiveLoginStatus = (data) => {
-    console.log('isLogin : ', data);
-    setLoginStatus(data);
-  }
-  // Login 페이지에서 App으로 사용자 이름을 넘겨줄 함수.
-  const receiveLoginID = (data) => {
-    console.log('whoLogin : ', data);
-    setwhoIsLogin(data);
-  }
-
-  // Login 페이지에서 App으로 쿠키 값을 넘겨줄 함수.
-  const receiveCookie = (data) => {
-    console.log('Cookie : ', data);
-    console.log('Cookie : ', cookies);
-    setCookie(data);
-    removeCookie();
-  }
-
   return (
     <> 
-      <myContext.Provider value={{ whoIsLogin, setwhoIsLogin, loginStatus, setLoginStatus }}>
-        <BrowserRouter>
-          <Uppermenu/>
-          <Routes>
-            <Route path='/' element={<Main/>}/>
-            <Route path='/api' element={<Callapi/>}/>
-            <Route path='/todolist' element={<Todolist/>}/>
-            <Route path='/board' element={<Board/>}/>
-            <Route path='/boardview/:BOARD_NUMBER' element={<BoardViewandMod/>}/>
-            <Route path='/login' element={<Login sendLoginStatus={receiveLoginStatus} sendLoginID={receiveLoginID} sendCookie={receiveCookie}/>}/>
-            <Route path='/join' element={<Join/>}/>
-            <Route path='/info' element={<MemberInfo/>}/>
+      <CookiesProvider>
+        <myContext.Provider value={{ whoIsLogin, setwhoIsLogin, loginStatus, setLoginStatus }}>
+          <BrowserRouter>
+            <Uppermenu/>
+            <Routes>
+              <Route path='/' element={<Main/>}/>
+              <Route path='/api' element={<Callapi/>}/>
+              <Route path='/todolist' element={<Todolist/>}/>
+              <Route path='/board' element={<Board/>}/>
+              <Route path='/boardview/:BOARD_NUMBER' element={<BoardViewandMod/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/join' element={<Join/>}/>
+              <Route path='/info' element={<MemberInfo/>}/>
 
-            <Route path='/test' element={<Test/>}/>
-          </Routes>
-        </BrowserRouter>
-      </myContext.Provider>
+              <Route path='/test' element={<Test/>}/>
+            </Routes>
+          </BrowserRouter>
+        </myContext.Provider>
+      </CookiesProvider>
     </>
   );
 }
