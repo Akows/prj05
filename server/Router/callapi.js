be32bb884ae390cc115ccaab309878601bccaa97
LeeYuSeung <akows141@gmail.com>
2022-09-07 23:09:23 +0900

    [공공데이터 API] - 공공데이터 API 호출 기능 구현 시작.

--- server/Router/callapi.js
+++ server/Router/callapi.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const request = require('request');
+
+router.get("/call", (req, res) => {
+
+    const options = {
+        uri: "http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo",
+
+    };
+
+    // const input = req.query.BOARD_TITLE;
+
+    var url = '';
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=서비스키'; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('xml'); /* */
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+    queryParams += '&' + encodeURIComponent('year') + '=' + encodeURIComponent('2020'); /* */
+    queryParams += '&' + encodeURIComponent('itemCode') + '=' + encodeURIComponent('PM10'); /* */
+
+    request(api_url, function(err, res, body){
+        console.log(body);
+    });
 
 
+
+
+})
 
 module.exports = router;
