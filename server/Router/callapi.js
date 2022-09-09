7fe3d8d45ab7189ce5ee5bfd329f29904173a407
LeeYuSeung <akows141@gmail.com>
2022-09-09 14:45:31 +0900

    [공공데이터 API] - 공공데이터 API '한국환경공단_에어코리아_미세먼지 경보 발령 현황'을 express와 request 모듈을 사용하여 정상 호출하는 기능을 구현.

--- server/Router/callapi.js
+++ server/Router/callapi.js
@@ -7,30 +7,32 @@ require("dotenv").config();
 
 router.get("/call", (req, res) => {
 
-    const skey = '=' + process.env.PUBLIC_API_CALLKEY;
+    // 발급받은 인증키를 저장한 env 파일에서 인증키를 가져온다.
+    const serviceKey = process.env.PUBLIC_API_CALLKEY;
 
-    const options = {
-        uri: "http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo",
+    // const options = {
+    //     uri: "http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo",
+    // };
 
-    };
-
-    // const input = req.query.BOARD_TITLE;
-
-    var url = '';
-    var queryParams = '?' + encodeURIComponent('serviceKey') + skey; /* Service Key*/
-    queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('xml'); /* */
+    var url = "http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo";
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* */
     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
     queryParams += '&' + encodeURIComponent('year') + '=' + encodeURIComponent('2020'); /* */
     queryParams += '&' + encodeURIComponent('itemCode') + '=' + encodeURIComponent('PM10'); /* */
+    
     const api_url = url + queryParams;
 
     request(api_url, function(error, res, body){
         if(error){
             console.log(error)
         }
-        var obj = JSON.parse(body)
-        console.log(obj)
+
+        console.log(body)
+
+        // var obj = JSON.parse(body)
+        // console.log(obj)
     });
 
 
