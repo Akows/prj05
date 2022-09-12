42acf434e7eecb56d3a52e0109a1374591ac6d87
LeeYuSeung <akows141@gmail.com>
2022-09-12 21:44:46 +0900

    [게시판 페이지] - 페이지 UI 개편. - 페이지네이션 기능 구현 시작.

--- client/src/components/boardlist.js
+++ client/src/components/boardlist.js
@@ -6,21 +6,14 @@ const BoardList = (props) => {
 
     return (
         <div className='board-contents-board'>
-            <div className='board-contents-boarddes'>
-                <div className='board-contents-boarddesnumber'>글번호</div>
-                <div className='board-contents-boarddestitle'>글제목</div>
-                <div className='board-contents-boarddeswriter'>작성자</div>
-                <div className='board-contents-boarddeswritetime'>작성시간</div>
-            </div>
-
             {props.boarddata && props.boarddata.datas?.map(item => {
                 return (
                     <div className='board-contents-boarddes' key={item.BOARD_NUMBER}>
-                        <div className='board-contents-boarddesnumber'>
+                        <div className='board-contents-boarddesnumber bod-conts-boddespubcss'>
                             {item.BOARD_NUMBER}
                         </div>
 
-                        <div className='board-contents-boarddestitle'>
+                        <div className='board-contents-boarddestitle bod-conts-boddespubcss'>
                             <Link to={`/boardview/${item.BOARD_NUMBER}`} state={{ BOARD_NUMBER: item.BOARD_NUMBER,
                                                                                     BOARD_TITLE: item.BOARD_TITLE,               
                                                                                     BOARD_TEXT: item.BOARD_TEXT,                
@@ -30,11 +23,11 @@ const BoardList = (props) => {
                             </Link>
                         </div>
 
-                        <div className='board-contents-boarddeswriter'>
+                        <div className='board-contents-boarddeswriter bod-conts-boddespubcss'>
                             {item.BOARD_WRITER}
                         </div>
 
-                        <div className='board-contents-boarddeswritetime'>
+                        <div className='board-contents-boarddeswritetime bod-conts-boddespubcss'>
                             {item.BOARD_WRITE_TIME}
                         </div>
 
--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -1,42 +1,128 @@
+import React, { useContext, useMemo, useState } from 'react';
+import { myContext } from '../App';
+import axios from 'axios';
+
 import './Test.css';
 import '../style/GlobalStyle.css';
 
+import BoardWrite from '../components/boardwrite';
+import BoardList from '../components/boardlist';
+import Pagination from '../components/pagination';
+
 const Test = () => {
+    const contextApi = useContext(myContext);
+
+    const [boarddata, setBoarddata] = useState([]);
+    const [componentvalue, setComponentvalue] = useState('list');
+
+    React.useEffect(() => {
+        axios
+        .get('/prj05/board/select')
+        .then(res => {
+            setBoarddata(res.data);
+            console.log(res.data.datas);
+        });
+    }, [])
+
+    const setTimeinfo = useMemo(e => {
+        let today = new Date();
+
+        let time = {
+            year: today.getFullYear(),  //현재 년도
+            month: today.getMonth() + 1, // 현재 월
+            date: today.getDate(), // 현제 날짜
+            // hours: today.getHours(), //현재 시간
+            // minutes: today.getMinutes(), //현재 분
+        };
+    
+        let timestring = `${time.year}년 ${time.month}월 ${time.date}일`;
+
+        return timestring;
+    }, []);
+
+    const showboardlist = () => {
+        setComponentvalue('list');
+    };
+
+    const showboardcreate = () => {
+        setComponentvalue('create');
+    };
+
+    // 페이지네이션 로직
+    // 페이지네이션 로직
+
+    // 현재 페이지를 제어할 변수 (가장 처음으로 보여질 페이지)
+    const [currentPage, setCurrentPage] = useState(1);
+    // 페이지 당 요소 갯수를 제어할 변수
+    const [postPerPage] = useState(5);
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const currentPosts = boarddata.datas?.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
         <>
-                <div className='tdl-pagebackground setcenter'>
-                    <div className='tdl-pageinner setcenter'>
-                        <div className='tdl-tdlarea setcenter gifont'>
+                <div className='bod-pagebackground setcenter'>
+                    <div className='bod-pageinner setcenter'>
+                        <div className='bod-bodarea setcenter gifont'>
 
-                            <div className='tdl-sidebar'>
+                            <div className='bod-sidebar'>
 
-                                <div className='tdl-sidetitle'>
-                                    <h1>TodoList(DB)</h1>
+                                <div className='bod-sidetitle'>
+                                    <h1>자유게시판</h1>
                                 </div>
 
-                                <div className='tdl-sideutil'>
-                                    
+                                <div className='bod-sideutil'>
+                                    {contextApi.loginStatus ? 
+                                        <h2>{contextApi.whoIsLogin}</h2>
+                                    :
+                                        <h2>익명(비로그인)</h2>
+                                    }
+
+                                    <h3>{setTimeinfo}</h3>
                                 </div>
 
-                                <div className='tdl-sidebtu'>
-                                    <button className='tdl-changebtu gifont'>No DB 버전으로 전환</button>
+                                <div className='bod-sidebtu'>
+                                    <button className='bod-changebtu gifont' onClick={showboardlist}>글 목록 보기</button>
+                                    <button className='bod-changebtu gifont' onClick={showboardcreate}>글 쓰기</button>
                                 </div>
 
                             </div>
 
-                            <div className='tdl-contents'>
-
-                                <div className='tdl-addtdl'>
-                                    
-                                </div>
-                                <div className='tdl-showtdl'>
-                                    <div className='todolist-loadbox'>
-                                        <h1>Loading...</h1>
+                            <div className='bod-contents'>
+                                <div className='bod-addbod'>
+                                    <div className='bod-contents-boarddes'>
+                                        <div className='bod-contents-boarddesnumber bod-conts-boddespubcss'>
+                                            <h3>글번호</h3>   
+                                        </div>
+                                        <div className='bod-contents-boarddestitle bod-conts-boddespubcss'>
+                                            <h3>제목</h3>
+                                        </div>
+                                        <div className='bod-contents-boarddeswriter bod-conts-boddespubcss'>
+                                            <h3>작성자</h3>
+                                        </div>
+                                        <div className='bod-contents-boarddeswritetime bod-conts-boddespubcss'>
+                                            <h3>작성시간</h3>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className='tdl-pagenation'>
-
+                                <div className='bod-showbod'>
+                                    {componentvalue === 'list' ? 
+                                        <BoardList boarddata={boarddata}/>
+                                    :
+                                        <BoardWrite whoLogin={contextApi.whoIsLogin}/>
+                                    }
+                                </div>
+                                <div className='bod-pagenation'>
+                                    <Pagination
+                                        postsPerPage={postPerPage}
+                                        totalPosts={currentPosts}
+                                        paginate={paginate}
+                                    />
                                 </div>
                             </div>
                         </div>
